bed85f291b324e479234c19ec3550e22d7105f5e
abhinavarvee <123144036+abhinavarvee@users.noreply.github.com>
2023-02-24 14:25:08 +0530

    Add files via upload
    assignment1 conditional statement usage

---
+++ conditional.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b ;
+    cout<<"Enter the first number: ";
+    cin>> a;
+    cout<<"Enter the second number: ";
+    cin>> b;
+    if(a==b){
+        cout<<a<<" is equal to "<<b;
+
+    }
+    else if (a>b){
+        cout<<a<<" is greater than "<<b;
+
+    }
+    else{
+        cout<<b<<" is greater than "<<a;
+
+    }
+    
+    
+    
+    
+}
